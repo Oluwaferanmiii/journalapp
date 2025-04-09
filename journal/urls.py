bc5d1f316af9c861a9ac058ed1e98dfb0a01e563
Feranmi Akinwale <feranmiphileo@gmail.com>
2025-04-10 01:12:55 +0200

    Update urls.py

--- journal/urls.py
+++ journal/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.create_entry_view, name="create_entry")
-    # path('login/', views.login_user, name="login")
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
